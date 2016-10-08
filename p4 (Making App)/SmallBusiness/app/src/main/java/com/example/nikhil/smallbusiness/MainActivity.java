fb179ef08be4093aa3ebb707be2b8688494388d0
Nikhil Bansal <nikhil97.nb@gmail.com>
2016-10-08 22:03:12 +0530

    Add files via upload

---
+++ p4 (Making App)/SmallBusiness/app/src/main/java/com/example/nikhil/smallbusiness/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.nikhil.smallbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "nikhil";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
